afe31a5c293cc0fc3f2f6d4d7dca9e7eafe3a022
Ali Serrano <aliperez1997@gmail.com>
2021-05-13 17:52:35 -0400

    practica django

--- e_commerce/e_commerce/views.py
+++ e_commerce/e_commerce/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html', {
-        #context
+        'mensaje' : 'Listado de Productos',
+        'titulo' : 'Productos',
+        'products' : [
+            {'titulo': 'Camisa' , 'precio' : 5, 'stock': True},
+            {'titulo': 'Pantalon' , 'precio' : 20, 'stock': True},
+            {'titulo': 'Correa' , 'precio' : 3, 'stock': False},
+            {'titulo': 'Chaqueta' , 'precio' : 15, 'stock': True},
+            {'titulo': 'Medias' , 'precio' : 1, 'stock': False}
+        ]
     })
