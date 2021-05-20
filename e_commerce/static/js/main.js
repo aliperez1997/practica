61b73030958c4231816d60624627110b329394ac
Ali Serrano <aliperez1997@gmail.com>
2021-05-20 19:12:41 -0400

    prueba de cdn de bootstrap

--- e_commerce/static/js/main.js
+++
@@ -1,8 +0,0 @@
-const xbtn = document.querySelector('#close');
-const alerta = document.querySelector('#alerta');
-
-xbtn.onclick = () => cerrarAlerta();
-
-function cerrarAlerta(){
-    alerta.remove();
-}
